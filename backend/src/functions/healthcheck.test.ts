0e7b6bf3e4f7e38144dce843dd0f5ef9d40236ee
Ke Duong <kelyduong@gmail.com>
2024-02-08 19:29:48 +1100

    add vitest to project

---
+++ backend/src/functions/healthcheck.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+
+describe.todo("Backend Healthcheck", () => {
+  it("should run correctly", () => {
+    const IamCorrect = true;
+    expect(IamCorrect).toBe(true);
+  });
+});
---
+++ client/src/App.test.tsx
@@ -0,0 +1,5 @@
+import { describe, test } from "vitest";
+
+describe("App test runs", () => {
+  test.todo("Run correctly", () => {});
+});
